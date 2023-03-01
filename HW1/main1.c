859596f4a36f2dc28a2887140ecad964169f7dbd
b1129058 <119778315+b1129058@users.noreply.github.com>
2023-03-01 16:55:44 +0800

    Create main1.c

---
+++ HW1/main1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0, num = 0, ct = 1;
+    char a[] = "AABBBACCCCddd";
+    char c = ' ';
+    
+    int len = sizeof(a)/sizeof(a[0])-1;
+    
+    for(i = 0;i<len;i++){
+        if(c != a[i]){
+            c = a[i];
+            printf("%c", a[i]);
+        }
+        if(a[i] != a[i+1]){
+            printf("%d", ct);
+            ct = 0;
+        }
+        ct++;
+    }
+
+    return 0;
+}
