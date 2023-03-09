454f7277076743ea1b2bd3e5657b2ce75ee32706
b1129058 <119778315+b1129058@users.noreply.github.com>
2023-03-09 14:07:30 +0800

    Update main.c

--- lab1/main.c
+++ lab1/main.c
@@ -6,19 +6,11 @@ int main ()
   FILE* fp;
   int a[] = { 1, 2, 3 };
   int arr_read[5];
-  //char b[] = "ABC";
-  //float c[] = { 1.1, 1.2, 1.3 };
-
-  if ((fp = fopen ("a.bin", "wb+")) == NULL)
-    {
-      exit (1);
-    }
+  fp = fopen ("a.bin", "wb+");
   fwrite (a, sizeof (a), 1, fp);
-  
-  
   fseek(fp, 0, SEEK_SET);
   fread(arr_read, sizeof(arr_read), 1, fp);
-
-
+  for(int i = 0;i<3;i++)
+    printf("%d", arr_read[i]);
   return 0;
 }
