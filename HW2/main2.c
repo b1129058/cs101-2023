57db89136691e12d06aad52a4141d51bde6a649b
b1129058 <119778315+b1129058@users.noreply.github.com>
2023-03-12 23:44:42 +0800

    Update main2.c

--- HW2/main2.c
+++ HW2/main2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(){
-	FILE  *fp= fopen("main2.c", "r+"), *fpw = fopen("main2.txt", "w+");
+	FILE  *fp= fopen("main.c", "r+"), *fpw = fopen("main2.txt", "w+");
 	int c, line_ct = 1;
 	
-	fprintf(fpw,"01 ");
-    while ( fread(&c, 1, 1, fp) == 1)
-        c == '\n' ? fprintf(fpw,"\n%02d ", ++line_ct) : fprintf(fpw,"%c", c);
+	fprintf(fpw,"01  ");
+	while ((c = getc(fp)) != EOF)
+	    c == '\n' ? fprintf(fpw,"\n%02d  ", ++line_ct) : fprintf(fpw,"%c", c);
 	fclose(fp);
 	fclose(fpw);
 	return 0;
